27d01e7e835438c38f92ab12c0e6221179b2b301
JedWoj <jedwojc@gmail.com>
2022-08-01 23:50:10 +0200

    migrating schemas to ts

--- web/my-app/pages/blog/index.tsx
+++ web/my-app/pages/blog/index.tsx
@@ -8,8 +8,8 @@ type Props = {
 
 const allBlogPostsPage = ({ posts }: Props) => {
     return (
-        <div>
-            <ul>
+        <div className='container mx-auto'>
+            <ul className='mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3'>
                 {posts.map(post => <Post key={post._id} post={post} />)}
             </ul>
         </div>
