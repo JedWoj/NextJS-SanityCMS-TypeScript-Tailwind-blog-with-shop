eb3bb40c231bd843756c7d48d4679d8b896b50f7
JedWoj <jedwojc@gmail.com>
2022-08-05 13:44:55 +0200

    cart icon added

--- web/my-app/pages/_app.tsx
+++ web/my-app/pages/_app.tsx
@@ -1,17 +1,19 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import Navbar from '../components/UI/Navbar';
+import CartIcon from '../components/cart/CartIcon';
 import { ShopCtxProvider } from '../store/shop-context';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ShopCtxProvider>
-      <div className='min-h-screen'>
+      <div className='min-h-screen relative'>
         <Navbar />
         <main>
           <Component {...pageProps} />
         </main>
       </div>
+      <CartIcon />
     </ShopCtxProvider>
   )
 }
